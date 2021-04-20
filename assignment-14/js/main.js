aeaf6061fea9440cbc1d657ce0ab2473a63b4b2c
Morgan Nydam <mozy123@gmail.com>
2021-04-19 22:23:05 -0600

    update

--- assignment-14/js/main.js
+++ assignment-14/js/main.js
@@ -2,30 +2,50 @@
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 ctx.moveTo(0, 0);
-ctx.lineTo(300, 100);
+ctx.lineTo(300, 2000);
 ctx.stroke();
 
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.moveTo(200, 100);
+ctx.lineTo(900, 1000);
+ctx.stroke();
 
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 ctx.beginPath();
-ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+ctx.arc(95, 350, 40, 0, 2 * Math.PI);
 ctx.stroke();
 
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(215, 170, 160, 120, 122 * Math.PI);
+ctx.stroke();
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.beginPath();
+ctx.arc(315, 170, 60, 20, 22 * Math.PI);
+ctx.stroke();
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "50px Arial";
+ctx.fillText("Hello World", 55, 95);
 
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
 ctx.font = "30px Arial";
-ctx.fillText("Hello World", 30, 60);
+ctx.fillText("Hello", 300, 195);
+
+var c = document.getElementById("canvas");
+var ctx = c.getContext("2d");
+ctx.font = "100px Arial";
+ctx.strokeText("Hello", 30, 80);
 
 
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
-ctx.font = "30px Arial";
-ctx.strokeText("Hello World", 10, 50);
-
-
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
-var img = document.getElementById("scream");
-ctx.drawImage(img, 10, 10);
+ctx.font = "100px Arial";
+ctx.strokeText("Hello", 150, 380);
---
+++ assignment-14/readme.md
@@ -0,0 +1,4 @@
+1. For my library I chose bootstrap. <br>
+2. I used a local file because that's what was easiest for me. <br>
+3. HTML5 changes the web by allowing more diverse and powerful websites and applications. <br>
+4. This week I wasn't entirely sure how to add frameworks, but was quickly able to solve it after asking for help.
